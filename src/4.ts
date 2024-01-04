a0ff3cc475ae0c3f8ba114640dc5379dea5a89a1
direct66612 <113045624+direct66612@users.noreply.github.com>
2024-01-04 01:33:55 -0500

    completed all tasks

--- src/4.ts
+++ src/4.ts
@@ -1,16 +1,54 @@
-// class Key {
-//   constructor(public signature: string) {}
-//   getSignature() {
-//     return this.signature;
-//   }
-// }
-// const key = new Key();
-// class Person {}
-// const house = new MyHouse(key);
-// const person = new Person(key);
+class Key {
+  private signature: number = Math.random();
+  getSignature(): number {
+    return this.signature;
+  }
+}
 
-// house.openDoor(person.getKey());
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
 
-// house.comeIn(person);
+  constructor(key: Key) {
+    this.key = key;
+  }
 
-// export {};
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} in house`);
+    } else {
+      console.log("no entry");
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("door is opened");
+    } else {
+      console.log("you are cannot opened door");
+    }
+  }
+}
+
+const key = new Key();
+const house = new MyHouse(key);
+const person = new Person(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
